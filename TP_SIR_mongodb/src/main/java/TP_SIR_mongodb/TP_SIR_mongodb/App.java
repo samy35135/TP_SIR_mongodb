c77ede77d7505e451fa9de42d5ae98f67a33ba1a
11002391 <11002391@e214m06.istic.univ-rennes1.fr>
2015-02-04 11:05:54 +0100

    Debut du projet

---
+++ TP_SIR_mongodb/src/main/java/TP_SIR_mongodb/TP_SIR_mongodb/App.java
@@ -0,0 +1,13 @@
+package TP_SIR_mongodb.TP_SIR_mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
